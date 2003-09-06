ca19cbd465c1272ba61471f4df2e5633d7ea238b
MatzeBraun <MatzeBraun@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2003-09-06 13:24:12 +0000

    only initialize SDL subsystems we really use

--- trunk/netpanzer/src/NetPanzer/Core/unix/main.cpp
+++ trunk/netpanzer/src/NetPanzer/Core/unix/main.cpp
@@ -5,7 +5,10 @@
 
 int main(int argc, char** argv)
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Init(SDL_INIT_TIMER 
+		   | SDL_INIT_VIDEO
+		 /*| SDL_INIT_AUDIO*/
+		   );
 
 	GameManager::initialize(argv[1]);
 
