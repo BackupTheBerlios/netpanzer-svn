f85c22c5a65ab76f43cc38bb7b7a5d99c9bffd37
MatzeBraun <MatzeBraun@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2003-09-11 18:56:54 +0000

    added a c++ wrapper for the physfs lib

---
+++ trunk/netpanzer/src/Lib/FileSystem.cpp
@@ -0,0 +1,344 @@
+/*
+Copyright (C) 2003 Matthias Braun <matze@braunis.de>
+                                                                                
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+                                                                                
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+                                                                                
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#include <config.h>
+
+#include <physfs.h>
+#include "Exception.hpp"
+#include "FileSystem.hpp"
+
+void FileSystem::initialize(const char* argv0)
+{
+	if (!PHYSFS_init(argv0))
+		throw Exception("failure while initialising physfs: %s",
+							PHYSFS_getLastError());
+
+	// TODO check search and writepaths here
+	PHYSFS_setSaneConfig("netpanzer", "netpanzer", "zip", 0, 0);
+}
+
+void FileSystem::shutdown()
+{
+	PHYSFS_deinit();
+}
+
+WriteFile* FileSystem::openWrite(const char* filename)
+{
+	PHYSFS_file* file = PHYSFS_openWrite(filename);
+	if(!file)
+		throw Exception("couldn't open file '%s' for writing: %s", filename,
+						PHYSFS_getLastError());
+	
+	return new WriteFile(file);
+}
+
+ReadFile* FileSystem::openRead(const char* filename)
+{
+	PHYSFS_file* file = PHYSFS_openRead(filename);
+	if(!file)
+		throw Exception("couldn't open file '%s' for reading: %s", filename,
+						PHYSFS_getLastError());
+
+	return new ReadFile(file);
+}
+
+WriteFile* FileSystem::openAppend(const char* filename)
+{
+	PHYSFS_file* file = PHYSFS_openAppend(filename);
+	if(!file)
+		throw Exception("couldn't open file '%s' for writing(append): %s",
+						filename, PHYSFS_getLastError());
+
+	return new WriteFile(file);
+}
+
+void FileSystem::mkdir(const char* directory)
+{
+	if(!PHYSFS_mkdir(directory))
+		throw Exception("couldn't create directory '%s': %s", directory,
+						PHYSFS_getLastError());
+}
+
+void FileSystem::remove(const char* filename)
+{
+	if(!PHYSFS_delete(filename))
+		throw Exception("couldn't remove file '%s': %s", filename,
+						PHYSFS_getLastError());
+}
+
+bool FileSystem::exists(const char* filename)
+{
+	return PHYSFS_exists(filename);
+}
+
+bool FileSystem::isDirectory(const char* filename)
+{
+	return PHYSFS_isDirectory(filename);
+}
+
+bool FileSystem::isSymbolicLink(const char* filename)
+{
+	return PHYSFS_isSymbolicLink(filename);
+}
+
+int64_t FileSystem::getLastModTime(const char* filename)
+{
+	int64_t modtime = PHYSFS_getLastModTime(filename);
+	if(modtime < 0)
+		throw Exception("couldn't determine modification time of '%s': %s",
+						filename, PHYSFS_getLastError());
+
+	return modtime;
+}
+
+//---------------------------------------------------------------------------
+
+File::File(PHYSFS_file* newfile)
+	: file(newfile)
+{
+}
+
+File::~File()
+{
+	PHYSFS_close(file);
+}
+
+bool File::eof()
+{
+	return PHYSFS_eof(file);
+}
+
+int64_t File::tell()
+{
+	return PHYSFS_tell(file);
+}
+
+void File::seek(uint64_t position)
+{
+	if(!PHYSFS_seek(file, position))
+		throw Exception("Seek operation failed: %s", PHYSFS_getLastError());
+}
+
+int64_t File::fileLength()
+{
+	return PHYSFS_fileLength(file);
+}
+
+void File::setBuffer(uint64_t bufsize)
+{
+	if(!PHYSFS_setBuffer(file, bufsize))
+		throw Exception("couldn't adjust buffer size: %s",
+						PHYSFS_getLastError());
+}
+
+void File::flush()
+{
+	PHYSFS_flush(file); // no exception - what should an app do if flush fails?
+}
+
+//---------------------------------------------------------------------------
+
+ReadFile::ReadFile(PHYSFS_file* file)
+	: File(file)
+{
+}
+
+int64_t ReadFile::read(void* buffer, size_t objsize, size_t objcount)
+{
+	return PHYSFS_read(file, buffer, objsize, objcount);
+}
+
+int16_t ReadFile::readSLE16()
+{
+	int16_t val;
+	if(!PHYSFS_readSLE16(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+uint16_t ReadFile::readULE16()
+{
+	uint16_t val;
+	if(!PHYSFS_readULE16(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+int16_t ReadFile::readSBE16()
+{
+	int16_t val;
+	if(!PHYSFS_readSBE16(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+uint16_t ReadFile::readUBE16()
+{
+	uint16_t val;
+	if(!PHYSFS_readUBE16(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+int32_t ReadFile::readSLE32()
+{
+	int32_t val;
+	if(!PHYSFS_readSLE32(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+uint32_t ReadFile::readULE32()
+{
+	uint32_t val;
+	if(!PHYSFS_readULE32(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+int32_t ReadFile::readSBE32()
+{
+	int32_t val;
+	if(!PHYSFS_readSBE32(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+uint32_t ReadFile::readUBE32()
+{
+	uint32_t val;
+	if(!PHYSFS_readUBE32(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+int64_t ReadFile::readSLE64()
+{
+	int64_t val;
+	if(!PHYSFS_readSLE64(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+uint64_t ReadFile::readULE64()
+{
+	uint64_t val;
+	if(!PHYSFS_readULE64(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+int64_t ReadFile::readSBE64()
+{
+	int64_t val;
+	if(!PHYSFS_readSBE64(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+uint64_t ReadFile::readUBE64()
+{
+	uint64_t val;
+	if(!PHYSFS_readUBE64(file, &val))
+		throw Exception("read error: %s", PHYSFS_getLastError());
+	return val;
+}
+
+//---------------------------------------------------------------------------
+
+WriteFile::WriteFile(PHYSFS_file* file)
+	: File(file)
+{
+}
+
+int64_t WriteFile::write(const void* buffer, size_t objsize, size_t objcount)
+{
+	return PHYSFS_write(file, buffer, objsize, objcount);
+}
+
+void WriteFile::writeSLE16(int16_t val)
+{
+	if(!PHYSFS_writeSLE16(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeULE16(uint16_t val)
+{
+	if(!PHYSFS_writeULE16(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeSBE16(int16_t val)
+{
+	if(!PHYSFS_writeSBE16(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeUBE16(uint16_t val)
+{
+	if(!PHYSFS_writeUBE16(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeSLE32(int32_t val)
+{
+	if(!PHYSFS_writeSLE32(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeULE32(uint32_t val)
+{
+	if(!PHYSFS_writeULE32(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeSBE32(int32_t val)
+{
+	if(!PHYSFS_writeSBE32(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeUBE32(uint32_t val)
+{
+	if(!PHYSFS_writeUBE32(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeSLE64(int64_t val)
+{
+	if(!PHYSFS_writeSLE64(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeULE64(uint64_t val)
+{
+	if(!PHYSFS_writeULE64(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeSBE64(int64_t val)
+{
+	if(!PHYSFS_writeSBE64(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
+void WriteFile::writeUBE64(uint64_t val)
+{
+	if(!PHYSFS_writeUBE64(file, val))
+		throw Exception("couldn't write: %s", PHYSFS_getLastError());
+}
+
---
+++ trunk/netpanzer/src/Lib/FileSystem.hpp
@@ -0,0 +1,126 @@
+/*
+Copyright (C) 2003 Matthias Braun <matze@braunis.de>
+                                                                                
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+                                                                                
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+                                                                                
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/** This is a thin C++ wrapper around libphysfs */
+#ifndef __LIB_FILESYSTEM_HPP__
+#define __LIB_FILESYSTEM_HPP__
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <physfs.h>
+
+//---------------------------------------------------------------------------
+
+class File
+{
+public:
+	~File();
+
+	bool eof();
+	int64_t tell();
+	void seek(uint64_t position);
+	int64_t fileLength();
+	// conveniance function, since I think this name is more c++ typic
+	int64_t length()
+	{ return fileLength(); }
+
+	void setBuffer(uint64_t bufsize);
+	void flush();
+
+protected:
+	File(PHYSFS_file* file);
+
+	PHYSFS_file* file;
+};
+
+//---------------------------------------------------------------------------
+
+class ReadFile : public File
+{
+public:
+	int64_t read(void* buffer, size_t objsize, size_t objcount);
+
+	int16_t readSLE16();
+	uint16_t readULE16();
+	int16_t readSBE16();
+	uint16_t readUBE16();
+
+	int32_t readSLE32();
+	uint32_t readULE32();
+	int32_t readSBE32();
+	uint32_t readUBE32();
+	
+	int64_t readSLE64();
+	uint64_t readULE64();
+	int64_t readSBE64();
+	uint64_t readUBE64();
+
+protected:
+	ReadFile(PHYSFS_file* file);
+	friend class FileSystem;
+};
+
+//---------------------------------------------------------------------------
+
+class WriteFile : public File
+{
+public:
+	int64_t write(const void* buffer, size_t objsize, size_t objcount);
+
+	void writeSLE16(int16_t val);
+	void writeULE16(uint16_t val);
+	void writeSBE16(int16_t val);
+	void writeUBE16(uint16_t val);
+
+	void writeSLE32(int32_t val);
+	void writeULE32(uint32_t val);
+	void writeSBE32(int32_t val); 	
+	void writeUBE32(uint32_t val);	
+
+	void writeSLE64(int64_t val);
+	void writeULE64(uint64_t val);
+	void writeSBE64(int64_t val);
+	void writeUBE64(uint64_t val);
+
+protected:
+	WriteFile(PHYSFS_file* file);
+	friend class FileSystem;
+};
+
+//---------------------------------------------------------------------------
+
+class FileSystem
+{
+public:
+	static void initialize(const char* argv0);
+	static void shutdown();
+	
+	static ReadFile* openRead(const char* filename);
+	static WriteFile* openAppend(const char* filename);
+	static WriteFile* openWrite(const char* filename);
+
+	static void mkdir(const char* dirname);
+	static void remove(const char* filename);
+	
+	static bool exists(const char* filename);
+	static bool isDirectory(const char* filename);
+	static bool isSymbolicLink(const char* filename);
+	static int64_t getLastModTime(const char* filename);
+};
+
+#endif
+
