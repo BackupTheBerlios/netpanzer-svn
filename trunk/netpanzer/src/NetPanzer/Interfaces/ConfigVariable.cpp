d84e115f7b2ecce2ecc92a20ad3f303acbccb31a
MatzeBraun <MatzeBraun@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2003-10-04 14:46:28 +0000

    new files

---
+++ trunk/netpanzer/src/NetPanzer/Interfaces/ConfigVariable.cpp
@@ -0,0 +1,72 @@
+#include <config.h>
+
+#include "Exception.hpp"
+#include "ConfigVariable.hpp"
+
+ConfigVariable::ConfigVariable(const std::string& newname)
+    : name(newname)
+{
+}
+
+ConfigVariable::~ConfigVariable()
+{
+}
+
+//---------------------------------------------------------------------------
+
+ConfigInt::ConfigInt(const std::string& newname, int newvalue,
+                     int newmin, int newmax)
+    : ConfigVariable(newname), min(newmin), max(newmax)
+{
+    *this = newvalue;
+}
+
+ConfigInt::~ConfigInt()
+{
+}
+
+int ConfigInt::operator = (int newvalue)
+{
+    if(newvalue < min || newvalue > max)
+        throw Exception("Configsetting '%s' out of range: %d (min: %d max: %d)",
+                getName().c_str(), newvalue, min, max);
+
+    value = newvalue;
+    return value;
+}
+
+//---------------------------------------------------------------------------
+
+ConfigBool::ConfigBool(const std::string& newname, bool newvalue)
+    : ConfigVariable(newname), value(newvalue)
+{
+}
+
+ConfigBool::~ConfigBool()
+{
+}
+
+bool ConfigBool::operator = (bool newvalue)
+{
+    value = newvalue;
+    return value;
+}
+
+//---------------------------------------------------------------------------
+
+ConfigString::ConfigString(const std::string& newname,
+                           const std::string& newvalue)
+    : ConfigVariable(newname), value(newvalue)
+{
+}
+
+ConfigString::~ConfigString()
+{
+}
+
+const std::string& ConfigString::operator= (const std::string& newvalue)
+{
+    value = newvalue;
+    return value;
+}
+
