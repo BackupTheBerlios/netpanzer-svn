bbad71dea3d4829e65d9cc0f4e6308c6402b08c6
MatzeBraun <MatzeBraun@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2003-11-29 16:57:59 +0000

    -fixed wrong read in blitWorld when we're rendering to lower right edge of the  map.

--- trunk/netpanzer/src/NetPanzer/Classes/TileEngine.cpp
+++ trunk/netpanzer/src/NetPanzer/Classes/TileEngine.cpp
@@ -65,14 +65,6 @@ void TileEngine::blitWorld()
 
     double_buffer = screen->mem;
 
-    /*
-       x_offset = (world_x % tile_size );
-       y_offset = (world_y % tile_size );
-       
-       x_length = tile_size - x_offset;
-       y_length = tile_size - y_offset;
-     */
-
     x_left_offset = (world_x % tile_size);
     x_left_length = (tile_size - x_left_offset);
 
@@ -121,19 +113,21 @@ void TileEngine::blitWorld()
     buffer_offset += x_left_length;
 
     for( i=0; i < complete_columns; i++ ) {
-        blit_partial_y( tile_offset, buffer_offset, y_top_length );
-
-        world_row++;
-
         tile_type = *(world_row);
 
         tile_offset = tile_set.getTile( tile_type );
         tile_offset = tile_offset + (y_top_offset * tile_size );
 
+        blit_partial_y( tile_offset, buffer_offset, y_top_length );
+
+        world_row++;
         buffer_offset += tile_size;
     }
 
     if( x_right_end_flag == true ) {
+        tile_type = *(world_row);
+        tile_offset = tile_set.getTile( tile_type );
+        tile_offset = tile_offset + (y_top_offset * tile_size );
 
         if (y_top_length > 0 && x_right_length > 0) {
             blit_partial_xy(tile_offset, buffer_offset, y_top_length, x_right_length);
@@ -151,7 +145,6 @@ void TileEngine::blitWorld()
     // THE MIDDLE ROWS
 
     for( k=0; k < complete_rows; k++) {
-
         tile_type = *(world_row);
 
         tile_offset = tile_set.getTile( tile_type );
@@ -165,16 +158,19 @@ void TileEngine::blitWorld()
         buffer_offset += x_left_length;
 
         for( i=0; i < complete_columns; i++ ) {
+            tile_type = *(world_row);
+            tile_offset = tile_set.getTile( tile_type );
+            
             blit_partial_y( tile_offset, buffer_offset, tile_size );
 
             world_row++;
-            tile_type = *(world_row);
-            tile_offset = tile_set.getTile( tile_type );
             buffer_offset += tile_size;
         }
 
 
         if( x_right_end_flag == true ) {
+            tile_type = *(world_row);
+            tile_offset = tile_set.getTile( tile_type );
             if (tile_size > 0 && x_right_length > 0) {
                 blit_partial_xy( tile_offset, buffer_offset, tile_size, x_right_length );
             }
@@ -210,17 +206,20 @@ void TileEngine::blitWorld()
         buffer_offset += x_left_length;
 
         for( i=0; i < complete_columns; i++ ) {
+            tile_type = *(world_row);
+            tile_offset = tile_set.getTile( tile_type );
+            
             if (y_bottom_length > 0) {
                 blit_partial_y( tile_offset, buffer_offset, y_bottom_length );
             }
             world_row++;
-
-            tile_type = *(world_row);
-            tile_offset = tile_set.getTile( tile_type );
             buffer_offset += tile_size;
         }
 
         if( x_right_end_flag == true ) {
+            tile_type = *(world_row);
+            tile_offset = tile_set.getTile( tile_type );
+            
             if (y_bottom_length > 0 && x_right_length > 0) {
                 blit_partial_xy( tile_offset, buffer_offset, y_bottom_length, x_right_length );
             }
