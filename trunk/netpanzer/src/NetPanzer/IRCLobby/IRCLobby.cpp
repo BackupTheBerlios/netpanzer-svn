0278d99a94d862ae14acbbd942775546ff209b55
matzebraun <matzebraun@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2004-01-17 13:51:45 +0000

    another fix

--- trunk/netpanzer/src/NetPanzer/IRCLobby/IRCLobby.cpp
+++ trunk/netpanzer/src/NetPanzer/IRCLobby/IRCLobby.cpp
@@ -26,7 +26,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #ifndef WITHOUT_NETPANZER
 
-#include <SDLNet.hpp>
 #include "Util/Log.hpp"
 #include "IRCLobbyView.hpp"
 #include "PlayerInterface.hpp"
