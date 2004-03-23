634fbce8bc0591f4314c3516610bfb93bd61001a
hollis <hollis@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2004-03-23 19:21:32 +0000

    include config.h here, otherwise WORDS_BIGENDIAN may not be set correctly

--- trunk/netpanzer/src/Lib/Util/Endian.hpp
+++ trunk/netpanzer/src/Lib/Util/Endian.hpp
@@ -19,6 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #define __ENDIAN_HPP__
 
 #include <stdint.h>
+#include <config.h>
 
 static inline uint16_t __swap16(uint16_t val)
 {
