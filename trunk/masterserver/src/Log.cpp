26d8f321f9c4fabc245e62165bc92a35bad2e576
matzebraun <matzebraun@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2004-10-04 15:54:00 +0000

    showtimestamps in server log

--- trunk/masterserver/src/Log.cpp
+++ trunk/masterserver/src/Log.cpp
@@ -19,7 +19,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "Log.hpp"
 
+#include <time.h>
 #include <ios>
+#include <iostream>
 #include <fstream>
 
 #include "Config.hpp"
@@ -27,29 +29,122 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 namespace masterserver
 {
 
+/** A streambuf implementation that wraps around an existing streambuf and
+ * outputs the current timestampe after each newline
+ */
+class FileStampStreamBuf : public std::streambuf
+{
+public:
+    FileStampStreamBuf(std::ostream& newstream)
+        : needstamp(true)
+    {
+        stream = &newstream;
+        setp(buf, buf+sizeof(buf));
+    }
+
+    virtual int overflow(int c)
+    {
+        if(pbase() == pptr())
+            return 0;
+       
+        if(needstamp)
+            outputdate();
+        needstamp = false;
+
+        // check for \n in the buffer
+        char* chunkstart = pbase();
+        for(char* p = pbase(); p != pptr(); ++p) {
+            if(*p == '\n') {
+                // output data in buffer so far
+                stream->write(chunkstart, p-chunkstart+1);
+                if(p < pptr()-1)
+                    outputdate();
+                else
+                    needstamp = true;
+                chunkstart = p+1;
+            }
+        }
+        // output the rest
+        stream->write(chunkstart, pptr() - chunkstart);
+
+        if(c != traits_type::eof()) {
+            *stream << (char) c;
+            if(c == '\n') {
+                needstamp = true;
+            }
+        }
+        setp(buf, buf+sizeof(buf));
+        return 0;
+    }
+
+    virtual int sync()
+    {
+        overflow(traits_type::eof());
+        stream->flush();
+        return 0;
+    }
+
+private:
+    void outputdate()
+    {
+        char timestamp[64];
+        time_t curtime = time(0);
+        struct tm* loctime = localtime(&curtime);
+        strftime(timestamp, sizeof(timestamp), "<%F %T>", loctime);
+        *stream << "\r" << timestamp;
+    }
+
+    std::ostream* stream;
+
+    bool needstamp;
+    char buf[1024];
+};
+
+class OFileStampStream : public std::ostream
+{
+public:
+    OFileStampStream(std::ostream& stream)
+        : std::ostream(new FileStampStreamBuf(stream))
+    { }
+
+    ~OFileStampStream()
+    {
+        delete rdbuf();
+    }
+};
+
 std::ostream* log = &std::cout;
+
+std::ostream* logfile = 0;
 
 void initializeLog()
 {
     // try to open a file
-    const std::string& logfile=config->getSection("server").getValue("logfile");
-    log = new std::ofstream(logfile.c_str(), std::ios_base::app);
-    if(!log->good()) {
+    const std::string& logfilename
+        = config->getSection("server").getValue("logfile");
+    
+    logfile = new std::ofstream(logfilename.c_str(), std::ios_base::app);
+    if(!logfile->good()) {
         std::cout << "Couldn't open '" << logfile
                   << "' falling back to stdout.\n";
-        delete log;
-        log = &std::cout;
+        delete logfile;
+
+        log = new OFileStampStream(std::cout);
     } else {
+        log = new OFileStampStream(*logfile);
         *log << "\nRestarted masterserver" << std::endl;
     }
 }
 
 void closeLog()
 {
-    if(log != &std::cout) {
+    if(logfile) {
         *log << "Logfile closed." << std::endl;
+        delete logfile;
+    }
+    if(log != &std::cout) {
         delete log;
-        log = 0;
+        log = &std::cout;
     }
 }
 
