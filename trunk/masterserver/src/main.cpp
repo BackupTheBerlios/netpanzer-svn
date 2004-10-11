60126a8160644eb89ddb236c48e7f83f9dd664d7
matzebraun <matzebraun@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2004-10-11 20:07:56 +0000

    missing assert.h

--- trunk/masterserver/src/main.cpp
+++ trunk/masterserver/src/main.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <iostream>
 #include <exception>
 #include <fstream>
+#include <assert.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
