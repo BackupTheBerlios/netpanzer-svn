ef4914c339862e3fc70ef4f0dc931d7ddc68e952
matzebraun <matzebraun@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2004-10-15 12:16:50 +0000

    missing header

--- trunk/netpanzer/src/Lib/Network/SocketSet.cpp
+++ trunk/netpanzer/src/Lib/Network/SocketSet.cpp
@@ -17,10 +17,11 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 #include <config.h>
 
+#include "SocketHeaders.hpp"
+
 #include <sstream>
 #include <stdexcept>
 
-#include "SocketHeaders.hpp"
 #include "SocketSet.hpp"
 
 namespace network
