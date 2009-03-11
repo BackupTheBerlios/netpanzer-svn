95122ac88b658559cacb65f43162b5fd8612e45e
kromxp <kromxp@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2009-03-11 15:06:04 +0000

    - Fix little scroll bug - Modify color of buttons, now doesn't looks ugly on the game.

--- trunk/netpanzer/src/NetPanzer/Classes/ViewCamera.cpp
+++ trunk/netpanzer/src/NetPanzer/Classes/ViewCamera.cpp
@@ -30,7 +30,7 @@ void ViewCamera::scrollPlusX( long scroll_increment )
     iXY map_size;
 
     view.x = loc.x + scroll_increment;
-    view.x = view.x - (view.x % 4);
+    view.x = view.x + (view.x % 4);
 
     getMapPointSize( &map_size );
 
@@ -75,7 +75,7 @@ void ViewCamera::scrollPlusY( long scroll_increment )
     iXY map_size;
 
     view.y = loc.y + scroll_increment;
-    view.y = view.y - (view.y % 4);
+    view.y = view.y + (view.y % 4);
 
     getMapPointSize( &map_size );
 
