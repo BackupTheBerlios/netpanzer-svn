e8c3d362efadaf624eaa0bdc3b21c2f75fa23811
kromxp <kromxp@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2009-06-28 04:11:34 +0000

    - created script to make Mac OS X app bundles and disk image files of netpanzer - created a icon (icns) for the app bundle - created a base image file for the script

---
+++ trunk/netpanzer/support/macosx/make_appbundle.sh
@@ -0,0 +1,75 @@
+#! /bin/bash
+
+RELEASEDIR="releases"
+APPNAME="NetPanzer"
+BASEDMG="support/macosx/basedmg.dmg"
+TEMPDMG="${RELEASEDIR}/tempdmg.dmg"
+FINALDMG="${RELEASEDIR}/NetPanzer.dmg"
+DMGROOT="${RELEASEDIR}/MacOS_DMGROOT"
+DMGDOC="${DMGROOT}/Documentation"
+APPDIR="${DMGROOT}/${APPNAME}.app"
+CONTENTSDIR="${APPDIR}/Contents"
+
+echo "Cleaning work folder"
+[ ! -d ${RELEASEDIR} ] && mkdir ${RELEASEDIR}
+
+rm -f ${TEMPDMG}
+rm -f ${FINALDMG}
+
+echo "Uncompressing and mounting dmg"
+hdiutil convert "${BASEDMG}" -format UDRW -o "${TEMPDMG}"
+hdiutil attach "${TEMPDMG}" -mountpoint "${DMGROOT}"
+
+mkdir "${CONTENTSDIR}" "${CONTENTSDIR}"/{Frameworks,MacOS,Resources}
+
+echo -n "Copying files"
+find cache maps pics powerups scripts sound units wads ! -type d  ! -path '*/.svn/*' ! -iname '.DS_Store' -print | \
+	while read fname; do 
+		DNAME="`dirname \"${fname}\"`"
+		[ ! -d "${CONTENTSDIR}/Resources/${DNAME}" ] && mkdir -p "${CONTENTSDIR}/Resources/${DNAME}"
+		cp "${fname}" "${CONTENTSDIR}/Resources/${fname}"
+		echo -n "."
+	done
+	
+cp BUGS ChangeLog COPYING LICENSE-physfs.txt README README-zlib123.txt RELNOTES TODO "${DMGDOC}"
+echo -n "."
+cp support/icon/netpanzer.icns "${CONTENTSDIR}/Resources"
+echo -n "."
+cp netpanzer "${CONTENTSDIR}/MacOS/"
+echo "."
+
+echo "Copying needed frameworks..."
+cp -R /Library/Frameworks/SDL{,_image}.framework "${CONTENTSDIR}/Frameworks"
+
+echo "Creating info files..."
+
+echo -n 'APPL????' > "${CONTENTSDIR}/PkgInfo"
+
+cat << EOF > "${CONTENTSDIR}/Info.plist"
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleDevelopmentRegion</key>
+	<string>English</string>
+	<key>CFBundleExecutable</key>
+	<string>netpanzer</string>
+	<key>CFBundleIconFile</key>
+	<string>netpanzer.icns</string>
+	<key>CFBundleInfoDictionaryVersion</key>
+	<string>6.0</string>
+	<key>CFBundlePackageType</key>
+	<string>APPL</string>
+	<key>CFBundleSignature</key>
+	<string>????</string>
+	<key>CFBundleVersion</key>
+	<string>0.0.1d1</string>
+</dict>
+</plist>
+EOF
+
+echo "Unmounting and creating final DMG"
+hdiutil detach "${DMGROOT}" -force
+hdiutil resize -sectors min "${TEMPDMG}"
+hdiutil convert "${TEMPDMG}" -format UDZO -o "${FINALDMG}" -imagekey zlib-level=9
+rm -f "${TEMPDMG}"
