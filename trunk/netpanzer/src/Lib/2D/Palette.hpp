68836ef603c93451a939164c2b7c1efe8eca2545
kromxp <kromxp@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2009-10-25 14:06:37 +0000

    - added missing include

--- trunk/netpanzer/src/Lib/2D/Palette.hpp
+++ trunk/netpanzer/src/Lib/2D/Palette.hpp
@@ -19,7 +19,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #define __Palette_hpp__
 
 #include <string>
-#include <stdio.h>
+#include <cstdio>
+#include <cassert>
 
 #include "Color.hpp"
 #include "Scripts/ScriptManager.hpp"
