07e0f9ffeb00b69082bb7f87fdc2a7b304a80b5a
kromxp <kromxp@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2010-01-04 18:34:46 +0000

    -Fixed case in include

--- trunk/netpanzer/src/NetPanzer/Particles/FlameParticle2D.cpp
+++ trunk/netpanzer/src/NetPanzer/Particles/FlameParticle2D.cpp
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "FlameParticle2D.hpp"
 #include "lua/lua.hpp"
 #include "2D/ImageArray.hpp"
-#include "util/Log.hpp"
+#include "Util/Log.hpp"
 #include "tolua++.h"
 
 typedef struct s_FlameArray
