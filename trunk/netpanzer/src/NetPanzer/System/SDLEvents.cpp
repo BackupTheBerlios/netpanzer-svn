3fa418a95b8bf88656efbe1d38d12417ec009366
kromxp <kromxp@0d1e1ef8-cfd4-0310-ad88-a35b9148bcce>
2012-01-13 10:02:05 +0000

    try fix for keyboards different than querty, awaiting wile test

--- trunk/netpanzer/src/NetPanzer/System/SDLEvents.cpp
+++ trunk/netpanzer/src/NetPanzer/System/SDLEvents.cpp
@@ -27,23 +27,56 @@
 #include "Interfaces/GameManager.hpp"
 #include "Util/Log.hpp"
 
-static SDLKey translate_key(SDLKey original)
+static SDLKey key_ascii_conversion[] =
 {
-    switch( original )
+    SDLK_a, SDLK_b, SDLK_c, SDLK_d, SDLK_e, SDLK_f, SDLK_g, SDLK_h, SDLK_i,
+    SDLK_j, SDLK_k, SDLK_l, SDLK_m, SDLK_n, SDLK_o, SDLK_p, SDLK_q, SDLK_r,
+    SDLK_s, SDLK_t, SDLK_u, SDLK_v, SDLK_w, SDLK_x, SDLK_y, SDLK_z
+};
+
+static SDLKey translate_key(SDL_keysym *keysym)
+{
+    SDLKey key = keysym->sym;
+    switch( key )
     {
-        case SDLK_AMPERSAND: return SDLK_1;
-        case SDLK_WORLD_73:  return SDLK_2;
-        case SDLK_QUOTEDBL:  return SDLK_3;
-        case SDLK_QUOTE:     return SDLK_4;
-        case SDLK_LEFTPAREN: return SDLK_5;
-        case SDLK_MINUS:     return SDLK_6;
-        case SDLK_WORLD_72:  return SDLK_7;
-        case SDLK_UNDERSCORE:return SDLK_8;
+        case SDLK_AMPERSAND:  return SDLK_1;
+        case SDLK_WORLD_73:   return SDLK_2;
+        case SDLK_QUOTEDBL:   return SDLK_3;
+        case SDLK_QUOTE:      return SDLK_4;
+        case SDLK_LEFTPAREN:  return SDLK_5;
+        case SDLK_MINUS:      return SDLK_6;
+        case SDLK_WORLD_72:   return SDLK_7;
+        case SDLK_UNDERSCORE: return SDLK_8;
         case SDLK_WORLD_71:   return SDLK_9;
         case SDLK_WORLD_64:   return SDLK_0;
         default:;
     }
-    return original;
+
+    if ( key >= SDLK_a && key <= SDLK_z && (keysym->unicode & 0xFF80) == 0 )
+    {
+        SDLKey unicode = (SDLKey)(keysym->unicode & 0x7F);
+        if ( unicode >= 'a' && unicode <= 'z' )
+        {
+            key_ascii_conversion[key-SDLK_a] = unicode;
+            return unicode;
+        }
+        else if ( unicode >= 'A' && unicode <= 'Z' )
+        {
+            unicode = (SDLKey)(unicode + ('a' - 'A'));
+            key_ascii_conversion[key-SDLK_a] = unicode;
+            return unicode;
+        }
+        else if ( unicode >= 1 && unicode <= 26 )
+        {
+            unicode = (SDLKey)(unicode + ('a' - 1));
+            key_ascii_conversion[key-SDLK_a] = unicode;
+            return unicode;
+        }
+
+        return key_ascii_conversion[key-SDLK_a];
+    }
+
+    return key;
 }
 
 bool handleSDLEvents()
@@ -67,7 +100,7 @@ bool handleSDLEvents()
             break;
         case SDL_KEYDOWN: {
 //                LOGGER.debug("Pressed key : scancode[%d] unicode[%d]", event.key.keysym.sym, event.key.keysym.unicode);
-                KeyboardInterface::keyPressed(translate_key(event.key.keysym.sym));
+                KeyboardInterface::keyPressed(translate_key(&event.key.keysym));
 
                 if ( (event.key.keysym.unicode & 0xFF80) == 0 )
                 {
@@ -92,7 +125,7 @@ bool handleSDLEvents()
             }
         case SDL_KEYUP:
 //            LOGGER.debug("Released key: scancode[%d] unicode[%d]", event.key.keysym.sym, event.key.keysym.unicode);
-            KeyboardInterface::keyReleased(translate_key(event.key.keysym.sym));
+            KeyboardInterface::keyReleased(translate_key(&event.key.keysym));
             break;
        
         case SDL_ACTIVEEVENT:
